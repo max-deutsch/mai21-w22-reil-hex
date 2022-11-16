43756631aca3f4d87c6f5a2bccf405d8df905dc3
Max Deutsch <max.deutsch@postoe.at>
2022-11-16 20:43:47 +0100

    feat(mcts): Add action to node and temporarily remove state from node. Add some comments

--- MonteCarloTreeSearch.py
+++ MonteCarloTreeSearch.py
@@ -8,19 +8,47 @@ Created on Thu Nov 10 15:50:06 2022
 from hex_engine import hexPosition
 
 class Node:
-    
+
+    # [
+    #   [0, 0]
+    #   [0, 0]
+    # ]
+
+    # n(s,a)
+    # Visit count per child.
+
+    # state s [0, 0, 0, 1]
+    # actions [(0,0), ..]
+
+
+
+    # TODO: invalid actions filtering
+
     def __init__(self):
-        self.state = None
-        self.visitCount = 0
-        self.value = 0
-        self.children ={}
+        self.parent = None
+        self.children = {}
+        self.action = None # The action taken to end up in this node e.g. (1,2)
+        self.visitCount = 0  # is needed for n(s,a)
+        self.accumulatedValue = 0 # is needed for w(s,a)
+
+        # self.state = None
+
+        # self.value = 0
+
+
+        # self.outcome # Only for leaf nodes. Storing the outcome of the game
+
         
     def isExpanded(self):
-        return return len(self.children) > 0
+        return len(self.children) > 0
     
     def Expand(self):
         #insert all valid actions fron that state into list of children
-        
+        pass
+
+    def determineActionWithUCT(self):
+        # for
+        return
         
 class MCTS:
     
@@ -33,12 +61,16 @@ class MCTS:
         #create tree with initial state
         root = Node()
         
-        for range(num_iterations):
+        for i in range(num_iterations):
             node = root
             
             #SELECTION
             while node.isExpanded():
                 #select a child and
+
+                # Determine a by UCT
+
+                pass
                 
             #invert Board?
             
