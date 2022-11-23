472fc492f3379964c2a0cf3e3de5b3f1849c4e15
PSchlagnitweit <74833687+PSchlagnitweit@users.noreply.github.com>
2022-11-23 16:29:59 +0100

    init(CNN): find sources

---
+++ CNN.py
@@ -0,0 +1,12 @@
+##### Sources #####
+# Repo of alpha zero approach with keras
+# https://github.com/likeaj6/alphazero-hex
+
+# Multi-Output CNN with pytorch on images
+# https://medium.com/jdsc-tech-blog/multioutput-cnn-in-pytorch-c5f702d4915f
+
+# use arrays in NN with pytorch
+# https://stackoverflow.com/questions/65017261/how-to-input-a-numpy-array-to-a-neural-network-in-pytorch
+# https://discuss.pytorch.org/t/input-numpy-ndarray-instead-of-images-in-a-cnn/18797/3
+
+#####         #####
