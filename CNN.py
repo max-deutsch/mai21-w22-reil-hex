a444bd39e6b8c108aea286b3541b365637e8449d
PSchlagnitweit <74833687+PSchlagnitweit@users.noreply.github.com>
2022-11-29 15:49:18 +0100

    feat(CNN): make batch_size independent

--- CNN.py
+++ CNN.py
@@ -45,7 +45,7 @@ class CustomDataset(Dataset):
         value = self.value[index]
         policy = self.policy[index]
 
-        sample = {'board': board,
+        sample = {'board': board.unsqueeze(0),
                   'value': value,
                   'policy': policy}
 
@@ -63,57 +63,59 @@ class CustomCNN(nn.Module):
     def __init__(self, num_row):
         super(CustomCNN, self).__init__()
         self.conv_layer1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3)
-        # self.batch_norm1 = nn.BatchNorm2d(num_features=32)
+        self.batch_norm1 = nn.BatchNorm2d(num_features=32)
         self.relu1 = nn.ReLU()
 
         self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        # self.batch_norm2 = nn.BatchNorm2d(32)
+        self.batch_norm2 = nn.BatchNorm2d(32)
         self.relu2 = nn.ReLU()
 
         # value output
         self.conv_layer3a = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        # self.batch_norm3a = nn.BatchNorm2d(32)
+        self.batch_norm3a = nn.BatchNorm2d(32)
         self.relu3a = nn.ReLU()
         self.fc3a = nn.Linear(32, 1)
 
         # policy output
         self.conv_layer3b = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
-        # self.batch_norm3a = nn.BatchNorm2d(32)
+        self.batch_norm3b = nn.BatchNorm2d(32)
         self.relu3b = nn.ReLU()
         self.fc3b = nn.Linear(32, num_row * num_row)
         self.softmax3b = nn.Softmax(dim=0)
 
-        # self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1)
-        # self.conv_layer4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1)
-        # self.max_pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
 
     # Progresses data across layers
     def forward(self, x):
+
+        # common CNN parts
         common = self.conv_layer1(x)
-        # common = self.batch_norm1(common)
+        common = self.batch_norm1(common)
         common = self.relu1(common)
 
         common = self.conv_layer2(common)
-        # common = self.batch_norm2(common)
+        common = self.batch_norm2(common)
         common = self.relu2(common)
 
         # value output
         value = self.conv_layer3a(common)
-        # value = self.batch_norm3a(value)
+        value = self.batch_norm3a(value)
         value = self.relu3a(value)
-        value = torch.flatten(value)
+        #value = torch.flatten(value)
+        value = value.reshape(value.size(0), -1)
         value = self.fc3a(value)
         value = torch.tanh(value)
 
         # policy output
         policy = self.conv_layer3b(common)
-        # policy = self.batch_norm3b(policy)
+        policy = self.batch_norm3b(policy)
         policy = self.relu3b(policy)
-        policy = torch.flatten(policy)
+        #policy = torch.flatten(policy)
+        policy = policy.reshape(policy.size(0), -1)
         policy = self.fc3b(policy)
         policy = self.softmax3b(policy)  # nn.LogSoftmax() might be more performant
 
-        return value, policy
+        return {'value': value, 'policy': policy}
 
 
 if __name__ == "__main__":
@@ -126,38 +128,36 @@ if __name__ == "__main__":
 
     for i in range(10):
         board_data = np.random.randint(low=-1, high=2, size=(7, 7))  # 7x7 board with values -1,0,1
+        board_x3 = np.dstack((board_data,board_data,board_data))  # duplicate channels
         board_array.append(board_data)
 
         value_data = np.random.random_sample()
         value_array.append(value_data)
 
-        policy_data = np.random.random_sample(5, )
+        policy_data = np.random.random_sample(3*3, )
         policy_array.append(policy_data)
 
     board_array = np.asarray(board_array)
     value_array = np.asarray(value_array)
     policy_array = np.asarray(policy_array)
 
-    # dataset = MyDataset(numpy_data, numpy_target)
-    # loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2, pin_memory=False)  # Running on CPU
-
-    # for i in range(len(board_array)):
-    #    print(board_array[i])
-    #    print(value_array[i])
-    #    print(policy_array[i])
 
     # test DataLoader with custom dataset
     dataset = CustomDataset(board_array, value_array, policy_array)
-    loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2, pin_memory=False)  # Running on CPU
+    loader = DataLoader(dataset, batch_size=10, shuffle=True, num_workers=2, pin_memory=False)  # Running on CPU
+
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(torch.cuda.is_available())
 
     CNN = CustomCNN(7)
 
-    for batch_idx, sample in enumerate(loader):
-        board = sample['board'].float().to(device)
-        value, policy = CNN(board)
-        print(value)
-        print(policy)
-        break
+    for batch_idx, sample_batched in enumerate(loader):
+        # importing data and moving to GPU
+        #print(sample_batched)
+        board, value, policy = sample_batched['board'].float().to(device), sample_batched['value'].to(device),  sample_batched['policy'].to(device)
+        #print(board.size())
+
+        # zero the parameter gradients
+        #optimizer.zero_grad()
+        output = CNN(board)
