bc22a081d0d160379cb04d63846911e9bec4f158
Max Deutsch <max.deutsch@postoe.at>
2023-01-04 15:33:39 +0100

    refactor(mcts): Remove unused code/comments

--- MonteCarloTreeSearch.py
+++ MonteCarloTreeSearch.py
@@ -2,14 +2,8 @@ import hex_engine as hex
 import math
 import copy
 import numpy as np
-from CNN import CustomCNN
-from CNN import CustomDataset
-from CNN import trainCNN
+
 from CNN import evalCNN
-import torch
-from torch import nn, optim
-from torch.utils.data import Dataset, DataLoader
-import random
 import time
 
 class Node:
@@ -42,30 +36,22 @@ class MCTS:
     def loop(self,root_node,game_state,device):
         
         game_state_copy = copy.deepcopy(game_state)
-        # print("loop start")
-        # game_state_copy.printBoard()
+
         current_node = root_node
         while True:
             if current_node.endReward:
                 break
 
             action = self.determine_action_by_uct(node=current_node, game_state=game_state_copy)
-            # print("got action: ", action)
             if action is None:
                 break
 
-            # take action, obtain reward (?), observe next state
             game_state_copy.board[action[0]][action[1]] = 1  # take action, always play as player 1 (white)
-            # print("took action as white: ", action)
-            # game_state_copy.printBoard()
             current_node.visitCount += 1
-            # flipping the board to continue playing as player 1 (white)
             game_state_copy.board = game_state_copy.recodeBlackAsWhite()
-            # print("flipped board")
-            # game_state_copy.printBoard()
+
             if action not in current_node.children:
                 next_node = Node(parent=current_node)  # adding current state to tree
-                # todo should visit count be increased?
                 current_node.children[action] = next_node
                 current_node = next_node
                 break
@@ -83,23 +69,16 @@ class MCTS:
         else:
             determine_results = evalCNN(CNN=self.model, game_state=game_state_copy,device=device)
             state_value = determine_results['value'].cpu()
-            #prior_probability = determine_results['policy']
             reward = state_value.detach().numpy()[0]  # convert tensor to value
 
-        # print("Got reward: ", reward)
-        # back propagate
         current_node.accumulatedValue += reward
         while current_node.parent is not None:
             current_node = current_node.parent
             reward = reward * -1
             current_node.accumulatedValue += reward
-        #print("MCTS run--- %s seconds ---" % (time.time() - run_start))
-        
         return
 
     def determine_action_by_uct(self, node: Node, game_state: hex.hexPosition):
-        #max_value = float('-inf')
-        #actions_max_value = []
         actions = []
         values = []
 
@@ -111,20 +90,10 @@ class MCTS:
             exploration_term = self.c * math.sqrt(math.log(node.visitCount)/action_count)
 
             v = mean_action_value + exploration_term
-            ## always take max
-            # if v > max_value:
-            #    actions_max_value = [action]
-            #    max_value=v
-            # elif v == max_value:
-            #    actions_max_value.append(action)
-            # choice = random.choice(actions_max_value)
-
-            ##probabilistic
             if type(v) == float:
                 value = v
             else:
                 value = v[0]
-
 
             actions.append(action)
             values.append(value)
@@ -168,33 +137,3 @@ class MCTS:
                 'value': value,
                 'policy': pi_array,
                 'board': game_state.board}
-
-
-# if __name__ == "__main__":
-#     board_size = 4
-#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#     CNN = CustomCNN(board_size).to(device)
-#     optimizer = optim.SGD(CNN.parameters(), lr=0.001, momentum=0.9)
-#     mcts = MCTS(model=CNN)
-#
-#     game_state = hex.hexPosition(board_size)
-#
-#     # run 16 MCTS and feed into CNN
-#     mcts_boards = []
-#     mcts_values = []
-#     mcts_policies = []
-#
-#     for i in range(16): # TODO this loop could be parallelized
-#         mcts_result = mcts.run(game_state=game_state, num_iterations=3, device=device)
-#         mcts_boards.append(np.asarray(game_state.board))
-#         mcts_values.append(mcts_result['value'])
-#         mcts_policies.append(mcts_result['policy'])
-#
-#     mcts_boards = np.asarray(mcts_boards)
-#     mcts_values = np.asarray(mcts_values)
-#     mcts_policies = np.asarray(mcts_policies)
-#
-#     train_set = CustomDataset(mcts_boards, mcts_values, mcts_policies)
-#     loader = DataLoader(train_set, batch_size=16, shuffle=True, num_workers=2, pin_memory=False)  # Running on CPU
-#     CNN.train()
-#     trainCNN(CNN, loader, optimizer, device)
